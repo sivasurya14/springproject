fde9b9e17c62f720605c6555577abb4a9bdbe3fd
sivasuriyan <sivasuriyan0120@gmail.com>
2023-07-28 14:59:48 +0530

    new commit

---
+++ src/main/java/com/application/gst/UseProductApplication.java
@@ -0,0 +1,13 @@
+package com.application.gst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseProductApplication.class, args);
+	}
+
+}
